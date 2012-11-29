ad714ffffb971a62f63c44a1d8b14382233de472
Hiroki Kondo <kompiro@gmail.com>
2012-11-29 16:12:36 +0900

    remove unused empty line

--- src/main/java/com/change_vision/astah/extension/plugin/mindmapplanner/view/ConnectionSettingDialog.java
+++ src/main/java/com/change_vision/astah/extension/plugin/mindmapplanner/view/ConnectionSettingDialog.java
@@ -25,7 +25,6 @@ import javax.swing.WindowConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
